d044f08e3007923eab6525779b90241f1d452caa
VALERIO Kevin <v16007689@A-F001925-L.salsa.univ-amu.fr>
2017-06-01 16:22:10 +0200

    Adding inscription and login

---
+++ src/ZenEtude/Utilisateur.java
@@ -0,0 +1,15 @@
+package ZenEtude;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+
+public class ControllerAbsenceNote {
+
+private static boolean ISCONNECTED = false;
+
+
+}
